835ab18c723f093dcb72e4fbf39a8aa7eaee4809
swetar-mecha <swetar@mechasystems.com>
2025-05-12 19:29:47 +0530

    core select update for key

--- crates/bevy_additional_core_widgets/src/core_select.rs
+++ crates/bevy_additional_core_widgets/src/core_select.rs
@@ -2,7 +2,8 @@ use accesskit::Role;
 use bevy::{
     a11y::AccessibilityNode,
     ecs::system::SystemId,
-    input_focus::{InputFocus, InputFocusVisible},
+    input::{ButtonState, keyboard::KeyboardInput},
+    input_focus::{FocusedInput, InputFocus, InputFocusVisible},
     prelude::*,
 };
 
@@ -52,7 +53,7 @@ pub struct CoreSelectContent {
     pub on_change: Option<SystemId<In<Entity>>>,
 }
 
-fn select_root_on_button_click(
+fn select_content_on_button_click(
     mut trigger: Trigger<ButtonClicked>,
     q_group: Query<(&CoreSelectContent, &Children)>,
     q_select_item: Query<(&Checked, &ChildOf, Has<InteractionDisabled>), With<CoreSelectItem>>,
@@ -99,15 +100,106 @@ fn select_root_on_button_click(
         .map(|(id, _)| *id);
 
     if current_select_item == Some(select_id) {
-        // If they clicked the currently checked radio button, do nothing
+        // If they clicked the currently checked item, do nothing
         return;
     }
 
-    // Trigger the on_change event for the newly checked radio button
+    // Trigger the on_change event for the newly checked item
     if let Some(on_change) = on_change {
         commands.run_system_with(*on_change, select_id);
     } else {
         commands.trigger_targets(ValueChange(select_id), group_id);
+    }
+}
+
+fn select_content_on_key_input(
+    mut trigger: Trigger<FocusedInput<KeyboardInput>>,
+    q_group: Query<(&CoreSelectContent, &Children)>,
+    q_select_item: Query<(&Checked, &ChildOf, Has<InteractionDisabled>), With<CoreSelectItem>>,
+    mut commands: Commands,
+) {
+    if let Ok((CoreSelectContent { on_change }, group_children)) = q_group.get(trigger.target()) {
+        let event = &trigger.event().input;
+        if event.state == ButtonState::Pressed
+            && !event.repeat
+            && matches!(
+                event.key_code,
+                KeyCode::ArrowUp
+                    | KeyCode::ArrowDown
+                    | KeyCode::ArrowLeft
+                    | KeyCode::ArrowRight
+                    | KeyCode::Home
+                    | KeyCode::End
+            )
+        {
+            let key_code = event.key_code;
+            trigger.propagate(false);
+
+            let select_children = group_children
+                .iter()
+                .filter_map(|child_id| match q_select_item.get(child_id) {
+                    Ok((checked, _, false)) => Some((child_id, checked.0)),
+                    Ok((_, _, true)) => None,
+                    Err(_) => None,
+                })
+                .collect::<Vec<_>>();
+
+            if select_children.is_empty() {
+                return; // No select items in the group
+            }
+            let current_index = select_children
+                .iter()
+                .position(|(_, checked)| *checked)
+                .unwrap_or(usize::MAX); // Default to invalid index if none are checked
+
+            let next_index = match key_code {
+                KeyCode::ArrowUp | KeyCode::ArrowLeft => {
+                    // Navigate to the previous select item in the group
+                    if current_index == 0 {
+                        // If we're at the first one, wrap around to the last
+                        select_children.len() - 1
+                    } else {
+                        // Move to the previous one
+                        current_index - 1
+                    }
+                }
+                KeyCode::ArrowDown | KeyCode::ArrowRight => {
+                    // Navigate to the next select item in the group
+                    if current_index >= select_children.len() - 1 {
+                        // If we're at the last one, wrap around to the first
+                        0
+                    } else {
+                        // Move to the next one
+                        current_index + 1
+                    }
+                }
+                KeyCode::Home => {
+                    // Navigate to the first select item in the group
+                    0
+                }
+                KeyCode::End => {
+                    // Navigate to the last select item in the group
+                    select_children.len() - 1
+                }
+                _ => {
+                    return;
+                }
+            };
+
+            if current_index == next_index {
+                // If the next index is the same as the current, do nothing
+                return;
+            }
+
+            let (next_id, _) = select_children[next_index];
+
+            // Trigger the on_change event for the newly checked select item
+            if let Some(on_change) = on_change {
+                commands.run_system_with(*on_change, next_id);
+            } else {
+                commands.trigger_targets(ValueChange(next_id), trigger.target());
+            }
+        }
     }
 }
 
@@ -142,7 +234,8 @@ pub struct CoreSelectPlugin;
 impl Plugin for CoreSelectPlugin {
     fn build(&self, app: &mut App) {
         app.add_observer(select_on_pointer_click)
-            .add_observer(select_root_on_button_click)
+            .add_observer(select_content_on_button_click)
+            .add_observer(select_content_on_key_input)
             .add_observer(select_item_on_pointer_click);
     }
 }
