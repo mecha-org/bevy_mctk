19251144d9013f6b43fd9ebe4e994082f4d941c8
swetar-mecha <swetar@mechasystems.com>
2025-05-14 19:58:14 +0530

    update: slider size styles

--- crates/bevy_styled_widgets/src/themes/input.rs
+++ crates/bevy_styled_widgets/src/themes/input.rs
@@ -17,3 +17,57 @@ impl InputStyle {
         }
     }
 }
+
+// Input size properties
+#[derive(Debug, Clone)]
+pub struct InputSizeProperties {
+    pub min_width: f32,
+    pub min_height: f32,
+    pub font_size: f32,
+    pub line_height: f32,
+}
+
+// Collection of size variants
+#[derive(Debug, Clone)]
+pub struct InputSizeStyles {
+    pub xsmall: InputSizeProperties,
+    pub small: InputSizeProperties,
+    pub medium: InputSizeProperties,
+    pub large: InputSizeProperties,
+    pub xlarge: InputSizeProperties,
+}
+
+pub fn input_sizes() -> InputSizeStyles {
+    InputSizeStyles {
+        xsmall: InputSizeProperties {
+            min_width: 60.0,
+            min_height: 36.0,
+            font_size: 12.0,
+            line_height: 16.0,
+        },
+        small: InputSizeProperties {
+            min_width: 80.0,
+            min_height: 36.0,
+            font_size: 14.0,
+            line_height: 18.0,
+        },
+        medium: InputSizeProperties {
+            min_width: 100.0, // 200.0,
+            min_height: 36.0, // 40.0,
+            font_size: 15.0,
+            line_height: 20.0,
+        },
+        large: InputSizeProperties {
+            min_width: 120.0,
+            min_height: 56.0,
+            font_size: 16.0,
+            line_height: 22.0,
+        },
+        xlarge: InputSizeProperties {
+            min_width: 144.0,
+            min_height: 56.0,
+            font_size: 18.0,
+            line_height: 24.0,
+        },
+    }
+}
